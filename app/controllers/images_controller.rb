7b2d2c365d574685232001924e9c3e078fb4dd5d
Shaun Hubbard <shaun@splife.com>
2016-07-28 17:22:59 -0600

    public folder

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
   def random
-    file_name = Dir.entries("#{Rails.root}/app/assets/images/tacos").select{|img| img =~ /(.*).jp?eg/}.sample
+    file_name = Dir.entries("#{Rails.root}/public/tacos").select{|img| img =~ /(.*).jp?eg/}.sample
     redirect_to ActionController::Base.helpers.asset_path("tacos/#{file_name}")
   end
 end
